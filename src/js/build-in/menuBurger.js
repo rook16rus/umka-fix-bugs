29a5e2c4ca199a1e92ee444825855c5b90ba3d03
Albert <kamalov00albert@gmail.com>
2022-07-06 09:56:22 +0300

    Поправил меню бургер

--- src/js/build-in/menuBurger.js
+++ src/js/build-in/menuBurger.js
@@ -4,14 +4,23 @@ export default function menuBurger() {
     const burger = document.querySelector('.header__burger')
     const menuBurger = document.querySelector('.menu-burger');
     const closeButton = document.querySelector('.menu-burger__close-button');
+    const twinkLogo = document.querySelector('.header.twink .header__logo');
 
     burger.addEventListener('click', () => {
         menuBurger.classList.add('active');
         disableScroll();
+
+        if (twinkLogo) {
+            twinkLogo.style.color = '#fff'
+        }
     })
 
     closeButton.addEventListener('click', () => {
         menuBurger.classList.remove('active');
         enableScroll();
+
+        if (twinkLogo) {
+            twinkLogo.style.color = '#1B74E3'
+        }
     })
 }
