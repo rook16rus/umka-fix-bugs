1c2eaafd2aa6da90cab4c2245c85158c00003212
rook16rus <kamalov00albert@gmail.com>
2022-07-27 16:23:44 +0300

    Правка

--- src/js/build-in/animateText.js
+++ src/js/build-in/animateText.js
@@ -15,7 +15,7 @@ export default function animateText() {
             header         = document.querySelector('.header'),
             tabsAnimate    = document.querySelector('.intro-slider'),
             videoBlock     = document.querySelector('.overview__inner'),
-            characterBtn   = document.querySelectorAll('.characters__button')
+            characterBtn   = document.querySelectorAll('.characters__button');
             
         window.addEventListener('DOMContentLoaded', () => {
             title.classList.add('animateTextWhite')
@@ -33,7 +33,7 @@ export default function animateText() {
 
         if (matchMedia('(max-height: 900px)').matches) {
             window.addEventListener('scroll', () => {
-                if(titleVideo[0].getBoundingClientRect().y <= 900) {
+                if(titleVideo && titleVideo[0].getBoundingClientRect().y <= 900) {
                     videoBlock.classList.add('active')
                     setTimeout(() => {
                         titleVideo.forEach(item=> {
@@ -41,41 +41,41 @@ export default function animateText() {
                         })
                     }, 2000);
                 }
-                if(titleConfig.getBoundingClientRect().y <= 800) {
+                if(titleConfig && titleConfig.getBoundingClientRect().y <= 800) {
                     titleConfig.querySelectorAll('span')[0].classList.add('animateTextBlack')
                     titleConfig.querySelectorAll('span')[1].classList.add('animateTextBlue')
                 }
-                if(titleUmka.getBoundingClientRect().y <= 650) {
+                if(titleUmka && titleUmka.getBoundingClientRect().y <= 650) {
                     titleUmka.classList.add('animateTextWhite')
                 }
-                if(titleWhy.getBoundingClientRect().y <= 730) {
+                if(titleWhy && titleWhy.getBoundingClientRect().y <= 730) {
                     titleWhy.classList.add('animateTextBlack')
                 }
-                if(titleGet.getBoundingClientRect().y <= 790) {
+                if(titleGet && titleGet.getBoundingClientRect().y <= 790) {
                     titleGet.classList.add('animateTextWhite')
                 }
-                if(titleMedia.getBoundingClientRect().y <= 750) {
+                if(titleMedia && titleMedia.getBoundingClientRect().y <= 750) {
                     titleMedia.classList.add('animateTextBlack')
                 }
-                if(titleCharacter.getBoundingClientRect().y <= 750) {
+                if(titleCharacter && titleCharacter.getBoundingClientRect().y <= 750) {
                     titleCharacter.classList.add('animateTextBlack')
                 }
-                if(titleReview.getBoundingClientRect().y <= 720){
+                if(titleReview && titleReview.getBoundingClientRect().y <= 720){
                     titleReview.classList.add('animateTextBlack')
                 }
-                if(titleOrder.getBoundingClientRect().y <= 690) {
+                if(titleOrder && titleOrder.getBoundingClientRect().y <= 690) {
                     titleOrder.classList.add('animateTextWhite')
                 }
-                if(titleCenter.getBoundingClientRect().y <= 690) {
+                if(titleCenter && titleCenter.getBoundingClientRect().y <= 690) {
                     titleCenter.classList.add('animateTextBlack')
                 }
-                if(titlePlant.getBoundingClientRect().y <= 680) {
+                if(titlePlant && titlePlant.getBoundingClientRect().y <= 680) {
                     titlePlant.classList.add('animateTextBlack')
                 }
             })
         } else {
             window.addEventListener('scroll', () => {
-                if(titleVideo[0].getBoundingClientRect().y <= 1200) {
+                if(titleVideo && titleVideo[0].getBoundingClientRect().y <= 1200) {
                     videoBlock.classList.add('active')
                     setTimeout(() => {
                         titleVideo.forEach(item=> {
@@ -83,35 +83,35 @@ export default function animateText() {
                         })
                     }, 2000);
                 }
-                if(titleConfig.getBoundingClientRect().y <= 1250) {
+                if(titleConfig && titleConfig.getBoundingClientRect().y <= 1250) {
                     titleConfig.querySelectorAll('span')[0].classList.add('animateTextBlack')
                     titleConfig.querySelectorAll('span')[1].classList.add('animateTextBlue')
                 }
-                if(titleUmka.getBoundingClientRect().y <= 1250) {
+                if(titleUmka && titleUmka.getBoundingClientRect().y <= 1250) {
                     titleUmka.classList.add('animateTextWhite')
                 }
-                if(titleWhy.getBoundingClientRect().y <= 1250) {
+                if(titleWhy && titleWhy.getBoundingClientRect().y <= 1250) {
                     titleWhy.classList.add('animateTextBlack')
                 }
-                if(titleGet.getBoundingClientRect().y <= 1250) {
+                if(titleGet && titleGet.getBoundingClientRect().y <= 1250) {
                     titleGet.classList.add('animateTextWhite')
                 }
-                if(titleMedia.getBoundingClientRect().y <= 1250) {
+                if(titleMedia && titleMedia.getBoundingClientRect().y <= 1250) {
                     titleMedia.classList.add('animateTextBlack')
                 }
-                if(titleCharacter.getBoundingClientRect().y <= 1250) {
+                if(titleCharacter && titleCharacter.getBoundingClientRect().y <= 1250) {
                     titleCharacter.classList.add('animateTextBlack')
                 }
-                if(titleReview.getBoundingClientRect().y <= 1250){
+                if(titleReview && titleReview.getBoundingClientRect().y <= 1250){
                     titleReview.classList.add('animateTextBlack')
                 }
-                if(titleOrder.getBoundingClientRect().y <= 1250) {
+                if(titleOrder && titleOrder.getBoundingClientRect().y <= 1250) {
                     titleOrder.classList.add('animateTextWhite')
                 }
-                if(titleCenter.getBoundingClientRect().y <= 1250) {
+                if(titleCenter && titleCenter.getBoundingClientRect().y <= 1250) {
                     titleCenter.classList.add('animateTextBlack')
                 }
-                if(titlePlant.getBoundingClientRect().y <= 1250) {
+                if(titlePlant && titlePlant.getBoundingClientRect().y <= 1250) {
                     titlePlant.classList.add('animateTextBlack')
                 }
             })
